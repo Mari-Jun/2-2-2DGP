481915389cce965ca92c0ea6a34f4d46db37496f
Mair-Jun <hatusimo0706@gmail.com>
2020-10-15 20:22:55 +0900

    이동 및 이미지 그리기

--- Project/Source/player.py
+++ Project/Source/player.py
@@ -2,12 +2,20 @@ from pico2d import *
 
 class Player:
 
+    keyMap = {
+        (SDL_KEYDOWN, SDLK_LEFT): (-1, 0),
+        (SDL_KEYDOWN, SDLK_RIGHT): (1, 0),
+        (SDL_KEYDOWN, SDLK_UP): (0, 1),
+        (SDL_KEYUP, SDLK_LEFT): (1, 0),
+        (SDL_KEYUP, SDLK_RIGHT): (-1, 0),
+        (SDL_KEYUP, SDLK_UP): (0, -1),
+    }
+
     game = None
     actions = ['Stop', 'Move', 'Jump', 'Attack', 'Die']
     imageSize = {'Stop': 30, 'Move': 27, 'Jump': 28, 'Attack': 29, 'Die': 29}
     imageIndex = {'Stop': 3, 'Move': 5, 'Jump': 8, 'Attack': 8, 'Die': 2}
-    images = {}
-    FPS = 12
+    images = { }
 
     def __init__(self, game):
         Player.game = game
@@ -15,7 +23,11 @@ class Player:
 
         self.images = Player.load_image('green')
         self.xPos = 100
+        self.xDelta = 0
         self.yPos = 100
+        self.yDelta = 0
+        self.speed = 200
+        self.time = 0
         self.imageIndex = 0
         self.action = 'Stop'
 
@@ -59,17 +71,29 @@ class Player:
         self.mGame.removeActor(self)
 
     def update(self):
-        #이미지 변환
-        self.imageIndex += 1
-        self.imageIndex %= Player.imageIndex[self.action]
+        # 이동
+        self.xPos += self.xDelta * self.speed * Player.game.deltaTime
+        self.yPos += self.yDelta * self.speed * Player.game.deltaTime
 
+        print('%d, %d' % (self.xPos, self.yPos))
+
+        #이미지 변환
+        self.time += Player.game.deltaTime
+        frame = self.time * 5
+        self.imageIndex = int(frame) % 5
+        self.imageIndex %= Player.imageIndex[self.action]
 
     def draw(self):
         image = self.images[self.action]
         startX = Player.imageSize[self.action] * self.imageIndex
-        image.clip_draw(startX, 0, Player.imageSize[self.action], 30, self.xPos, self.yPos)
+        filp = 'h' if self.xDelta < 0 else ''
+        image.clip_composite_draw(startX, 0, Player.imageSize[self.action], 30, 0, filp,
+                                  self.xPos, self.yPos, Player.imageSize[self.action], 30)
         pass
 
     def processInput(self, key):
-        pass
+        pair = (key.type, key.key)
+        if pair in Player.keyMap:
+            self.xDelta += Player.keyMap[pair][0]
+            self.yDelta += Player.keyMap[pair][1]
 
