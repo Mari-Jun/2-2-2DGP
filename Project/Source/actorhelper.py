4bd93ed5d55e7e39f636826708606711f58847eb
Mair-Jun <hatusimo0706@gmail.com>
2020-10-18 00:33:59 +0900

    ActorHelper 함수 생성

---
+++ Project/Source/actorhelper.py
@@ -0,0 +1,52 @@
+from pico2d import *
+import physics
+
+def commomUpdate(actor):
+    # 중력 설정
+    if actor.mYDelta > -5:
+        actor.mYDelta -= 0.125
+
+    # 이동
+    xMove = actor.mXDelta * actor.mSpeed * actor.page.mGame.deltaTime
+    yMove = actor.mYDelta * actor.mSpeed / 2 * actor.page.mGame.deltaTime
+
+    # 충돌 검사
+    actor.mXPos += xMove
+    for block in actor.page.map.sideBlocks:
+        if physics.collidesBlock(actor, block):
+            actor.mXPos -= xMove
+            break
+
+    if actor.mAction != 'Jump':
+        for block in actor.page.map.blocks:
+            if physics.collidesBlock(actor, block):
+                actor.mXPos -= xMove
+                break
+
+    actor.mYPos += yMove
+    for block in actor.page.map.blocks:
+        if physics.collidesBlock(actor, block) and actor.mYDelta < 0:
+            actor.mYPos -= yMove
+            actor.mYDelta = 0
+            actor.mAction = 'Stop' if actor.mXDelta == 0 else 'Move'
+            break
+
+    # 액션 설정
+    if actor.mAction != 'Attack' and actor.mAction != 'Jump':
+        actor.mAction = 'Stop' if actor.mXDelta == 0 else 'Move'
+
+    # 이미지 변환
+    actor.mTime += actor.page.mGame.deltaTime
+    actor.mImageIndex = int(actor.mTime * 10)
+
+    # 액션 재설정
+    if actor.mAction == 'Attack' and actor.mImageIndex > actor.imageIndexs['Attack']:
+        actor.mAction = 'Stop' if actor.mXDelta == 0 else 'Move'
+
+    actor.mImageIndex %= actor.imageIndexs[actor.mAction]
+
+def commomDraw(actor):
+    image = actor.mImages[actor.mAction]
+    startX = image.w // actor.imageIndexs[actor.mAction] * actor.mImageIndex
+    image.clip_composite_draw(startX, 0, image.w // actor.imageIndexs[actor.mAction], image.h, 0, actor.mFlip,
+                              actor.mXPos, actor.mYPos, image.w // actor.imageIndexs[actor.mAction], image.h)
--- Project/Source/player.py
+++ Project/Source/player.py
@@ -1,8 +1,8 @@
 from pico2d import *
 import physics
+import actorhelper
 
 class Player:
-
     keyMap = {
         (SDL_KEYDOWN, SDLK_LEFT):  (-1,  0),
         (SDL_KEYDOWN, SDLK_RIGHT): ( 1,  0),
@@ -14,23 +14,22 @@ class Player:
 
     page = None
     actions = ['Stop', 'Move', 'Jump', 'Attack', 'Die']
-    imageIndex = {'Stop': 3, 'Move': 5, 'Jump': 8, 'Attack': 4, 'Die': 2}
+    imageIndexs = {'Stop': 3, 'Move': 5, 'Jump': 8, 'Attack': 4, 'Die': 2}
     images = { }
 
     def __init__(self, page):
         Player.page = page
         self.load()
-
-        self.images = Player.load_image('green')
-        self.xPos = 100
-        self.xDelta = 0
-        self.yPos = 110
-        self.yDelta = -5
-        self.flip = ''
-        self.speed = 200
-        self.time = 0
-        self.imageIndex = 0
-        self.action = 'Stop'
+        self.mImages = Player.load_image('green')
+        self.mXPos = 100
+        self.mXDelta = 0
+        self.mYPos = 110
+        self.mYDelta = -5
+        self.mFlip = ''
+        self.mSpeed = 200
+        self.mTime = 0
+        self.mImageIndex = 0
+        self.mAction = 'Stop'
 
     def __del__(self):
         pass
@@ -72,86 +71,38 @@ class Player:
         self.removeActor(self)
 
     def update(self):
-        #중력 설정
-        if self.yDelta > -5:
-            self.yDelta -= 0.125
-
-        # 이동
-        xMove = self.xDelta * self.speed * Player.page.mGame.deltaTime
-        yMove = self.yDelta * self.speed / 2 * Player.page.mGame.deltaTime
-
-        #충돌 검사
-        self.xPos += xMove
-        for block in Player.page.map.sideBlocks:
-            if physics.collidesBlock(self, block):
-                self.xPos -= xMove
-                break
-
-        if self.action != 'Jump':
-            for block in Player.page.map.blocks:
-                if physics.collidesBlock(self, block):
-                    self.xPos -= xMove
-                    break
-
-        self.yPos += yMove
-        for block in Player.page.map.blocks:
-            if physics.collidesBlock(self, block) and self.yDelta < 0:
-                self.yPos -= yMove
-                self.yDelta = 0
-                self.action = 'Stop' if self.xDelta == 0 else 'Move'
-                break
-
-        # 액션 설정
-        if self.action != 'Attack' and self.action != 'Jump':
-            self.action = 'Stop' if self.xDelta == 0 else 'Move'
-
-        #이미지 변환
-        self.time += Player.page.mGame.deltaTime
-        self.imageIndex = int(self.time * 10)
-
-        #액션 재설정
-        if self.action == 'Attack' and self.imageIndex > Player.imageIndex['Attack']:
-            self.action = 'Stop' if self.xDelta == 0 else 'Move'
-
-
-        self.imageIndex %= Player.imageIndex[self.action]
+        actorhelper.commomUpdate(self)
 
     def draw(self):
-        print(self.action)
-        image = self.images[self.action]
-        startX = image.w // Player.imageIndex[self.action] * self.imageIndex
-        image.clip_composite_draw(startX, 0, image.w // Player.imageIndex[self.action], image.h, 0, self.flip,
-                                  self.xPos, self.yPos, image.w // Player.imageIndex[self.action], image.h)
+        actorhelper.commomDraw(self)
 
     def processInput(self, key):
         pair = (key.type, key.key)
         if pair in Player.keyMap:
-            self.xDelta += Player.keyMap[pair][0]
-            if self.xDelta < 0:
-                self.flip = 'h'
-            elif self.xDelta > 0:
-                self.flip = ''
+            self.mXDelta += Player.keyMap[pair][0]
+            if self.mXDelta < 0:
+                self.mFlip = 'h'
+            elif self.mXDelta > 0:
+                self.mFlip = ''
         elif pair == Player.KEYDOWN_ATTACK:
             self.attack()
         elif pair == Player.KEYDOWN_JUMP:
             self.jump()
 
-
     def attack(self):
-        if not self.action == 'Attack':
-            self.time = 0
-            self.imageIndex = 0
-            self.action = 'Attack'
+        if not self.mAction == 'Attack':
+            self.mTime = 0
+            self.mImageIndex = 0
+            self.mAction = 'Attack'
 
     def jump(self):
-        if not self.action == 'Jump':
-            self.time = 0
-            self.imageIndex = 0
-            self.action = 'Jump'
-            self.yDelta = 5
+        if not self.mAction == 'Jump':
+            self.mTime = 0
+            self.mImageIndex = 0
+            self.mAction = 'Jump'
+            self.mYDelta = 5
 
     def getBB(self):
-        hw = self.images['Stop'].w // Player.imageIndex['Stop'] / 2 - 10
-        hh = self.images['Stop'].h / 2 - 10
-        return self.xPos - hw, self.yPos - hh, self.xPos + hw, self.yPos + hh
-
+        hw = self.mImages['Stop'].w // Player.imageIndexs['Stop'] / 2 - 10
+        hh = self.mImages['Stop'].h / 2 - 10
+        return self.mXPos - hw, self.mYPos - hh, self.mXPos + hw, self.mYPos + hh
